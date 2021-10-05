c87ba75417c4d6325d66de65e9a1c7c7e14b4206
James Leal Vieira <james.leal2@gmail.com>
2021-10-04 21:48:01 -0300

    LoginPage

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,49 @@
+import {
+  InputProps as ChakraInputProps,
+  Input as ChakraInput,
+  FormLabel,
+  FormErrorMessage,
+  FormControl,
+} from '@chakra-ui/react';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
+import { FieldError } from 'react-hook-form';
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  register?: Function;
+  req?: string;
+  error?: FieldError;
+}
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, register = () => {}, req, error = null, ...rest },
+  ref
+) => {
+  return (
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        bg="gray.700"
+        variant=""
+        _hover={{
+          bg: 'gray.700',
+        }}
+        _focus={{
+          bg: 'gray.700',
+          border: '1.5px solid #AC2044',
+        }}
+        _placeholder={{
+          color: 'gray.400',
+        }}
+        size="lg"
+        {...register(name, { required: req })}
+        {...rest}
+      />
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+    </FormControl>
+  );
+};
+
+export const Input = forwardRef(InputBase);
---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      400: '#5B5F61',
+      500: '#53575A',
+      700: '#353739',
+      800: '#202327',
+      900: '#1B1D1F',
+    },
+    pink: {
+      600: '#AC2044',
+      500: '#C4244D',
+    },
+    green: {
+      500: '#00C439',
+    },
+    white: '#E5E5E5',
+  },
+  fonts: {
+    body: 'Sora, sans-serif',
+    heading: 'Sora, sans-serif',
+  },
+
+  styles: {
+    global: {
+      'html, body': {
+        bg: 'gray.900',
+        color: 'white',
+      },
+    },
+  },
+});
